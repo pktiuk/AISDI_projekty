e6395c1ffef4dd0f5808a17acbd41cc35a8a6f6c
pktiuk <kotiuk@wp.pl>
2019-11-15 11:25:27 +0100

    Move bubblesort() to .hpp

--- Projekt2/include/bubblesort.hpp
+++ Projekt2/include/bubblesort.hpp
@@ -3,5 +3,20 @@
 #include <string>
 
 
-template<typename IteratorType>
-void bubbleSort(IteratorType first, IteratorType last);
+template <typename IteratorType> 
+void bubbleSort(IteratorType first, IteratorType last)
+{
+  bool sorted = false;
+  while (!sorted)
+  {
+    sorted=true;
+    for (auto current=next(first,0); current != last; current++)
+    {
+      if ((*current) > (*(current + 1)))
+      {
+        std::iter_swap(current, current + 1);
+        sorted=false;
+      }
+    }
+  }
+}
