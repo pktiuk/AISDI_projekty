6c3adafe8fc39d71f9a817e4905a5212a822e4c6
pktiuk <kotiuk@wp.pl>
2019-12-11 15:38:23 +0100

    Loading in main

--- Projekt3/src/main.cpp
+++ Projekt3/src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <TreeMap.hpp>
 #include <loadWords.hpp>
+#include <load.hpp>
+
+
+using namespace std;
 
 int main()
 {
-    //TreeMap<int,int> tree = TreeMap<int, int>();
+    TreeMap<string,int> tree = read2tree("pan-tadeusz.txt");
 
     //tree.insert(0,0);
     //write(tree.root);
