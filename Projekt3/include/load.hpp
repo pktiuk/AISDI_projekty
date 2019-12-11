d5a83fede411467aa3d577ef57ccee6b2461da45
pktiuk <kotiuk@wp.pl>
2019-12-11 15:34:47 +0100

    Add load2tree

---
+++ Projekt3/include/load.hpp
@@ -0,0 +1,33 @@
+#include <TreeMap.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+TreeMap<string, int> read2tree(string filename, int num = INT32_MAX)
+{
+  int i = 0;
+  ifstream inFile;
+
+  TreeMap<string, int> tree = TreeMap<string, int>();
+
+  inFile.open(filename);
+
+  if (!inFile)
+  {
+    cout << "Unable to open file";
+    exit(1); // terminate with error
+  }
+
+  string word;
+
+  while (inFile >> word && i < num)
+  {
+    tree.insert(word, i);
+    i++;
+  }
+
+  return tree;
+}
