ee5186e02a64760171e1caa647ef2ac6582bc685
pktiuk <kotiuk@wp.pl>
2019-12-12 20:39:48 +0100

    Add tests to main

--- Projekt3/src/main.cpp
+++ Projekt3/src/main.cpp
@@ -1,12 +1,14 @@
 #include <TreeMap.hpp>
+#include <loadWords.hpp>
 #include <iostream>
 #include <load.hpp>
-#include <loadWords.hpp>
 #include <map>
+#include <random>
 
 using namespace std;
 
-int main()
+
+void loadTest()
 {
   size_t sysTime, myTime;
   for (int i = 10; i < 50000; i = i + 100)
@@ -19,6 +21,31 @@ int main()
     sysTime = b2.elapsed();
     cout << i << " " << myTime << " " << sysTime << endl;
   }
+}
 
+void searchTest()
+{
+  size_t sysTime, myTime;
+  int num;
+  for (int i = 10; i < 50000; i = i + 100)
+  {
+    TreeMap<string, int> tree = read2tree("pan-tadeusz.txt", i);
+    map<string, int> sysMap = read2map("pan-tadeusz.txt", i);
+    num= random()%i;
+    std::vector<string> vec=read("pan-tadeusz.txt", i);
+    Benchmark<std::chrono::nanoseconds> b;
+    tree.value(vec[num]);
+    myTime = b.elapsed();
+    Benchmark<std::chrono::nanoseconds> b2;
+    sysMap.find(vec[num]);
+    sysTime = b2.elapsed();
+    cout << i << " " << myTime << " " << sysTime << endl;
+  }
+}
+
+int main()
+{
+  
+  searchTest();
   return 0;
 }
