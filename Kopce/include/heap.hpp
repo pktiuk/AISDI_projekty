c3705eb51fe9e7ea1ed806f9744f6da1356ffcd8
tomsloj <tomsloj@gmail.com>
2019-12-12 22:24:53 +0100

    height

--- Kopce/include/heap.hpp
+++ Kopce/include/heap.hpp
@@ -1,5 +1,7 @@
 #include <utility>
+#include <vector>
 
+using namespace std;
 
 template <typename KeyType, typename ValueType>
 class Heap4
@@ -17,7 +19,30 @@ public:
     KeyValueType pop() noexcept;
     size_t size() const noexcept;
 
+    vector<KeyValueType>heap;
+
+    int getHeight();
+
+    template<typename StreamType> void print(StreamType& stream) const;
+
     Heap4(/* args */){};
     ~Heap4(){};
 };
+
+template <typename KeyType, typename ValueType>
+int Heap4<KeyType, ValueType>::getHeight()
+{
+    if(heap.empty())
+        return 0;
+    int height = 1;
+    int i = 0;
+    while( i < heap.size()-1 )
+    {
+        i = i * 4 + 4 ;
+        height ++;
+    }
+    return height;
+}
+
+
 
