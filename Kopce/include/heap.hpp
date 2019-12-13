a3a57671e385450bf83f72f8e0a76c26cb8a3b8c
tomsloj <tomsloj@gmail.com>
2019-12-13 11:41:01 +0100

    works

--- Kopce/include/heap.hpp
+++ Kopce/include/heap.hpp
@@ -3,51 +3,77 @@
 
 #include <utility>
 #include <vector>
+#include<stdexcept>
+	
 
 using namespace std;
 
-int const D = 4;
+int const D = 3;
 
 template <typename KeyType, typename ValueType>
-class Heap4
+class Heap3
 {
 public:
-    using KeyValueType = std::pair<const KeyType, ValueType>;
+    using KeyValueType = std::pair<KeyType, ValueType>;
 
     bool empty() const noexcept
     {
-        return true;
+        if(heap.size() == 0)
+            return true;
+        return false;
     }
+
     void insert(KeyType const & key, ValueType const & value);
     void insert(KeyValueType const & key_value);
-    KeyValueType const & peek() const noexcept;
+    KeyValueType const & peek() const
+    {
+        if(!heap.empty())
+            return heap[0];
+        else
+        {
+            std::logic_error e("messag");
+            throw e;
+        }
+    }
     KeyValueType pop() noexcept;
-    size_t size() const noexcept;
+    size_t size() const noexcept
+    {
+        return heap.size();
+    }
 
     vector<pair<KeyType, ValueType> >heap;
 
     int getHeight();
 
-    template<typename StreamType> void print(StreamType& stream) const;
+    template<typename StreamType> void print(StreamType& stream) const
+    {
 
-    Heap4(/* args */){};
-    ~Heap4(){};
+    }
+
+    Heap3(/* args */){};
+    ~Heap3(){};
 };
 
 template <typename KeyType, typename ValueType>
-std::pair<const KeyType, ValueType> Heap4<KeyType, ValueType>::pop() noexcept
+std::pair<KeyType, ValueType> Heap3<KeyType, ValueType>::pop() noexcept
 {
     pair<const KeyType, ValueType> result = heap[0];
     heap[0] = heap[heap.size() - 1];
     heap.pop_back();
     int current = 0;
-    while( current*D+D < heap.size() )
+    while( current*D + 1 < heap.size() )
     {
-        int maksimum = max(max(heap[current*D+1], heap[current*D +2]), max(heap[current*D+3], heap[current*D +4]));
-        if(maksimum <= heap[current])
+        int minimum = heap[current*D+1].first;
+        for( int i = 1; i < 3; ++i )
+        {
+            if(current*D + i < heap.size())
+                minimum = min(minimum, heap[current*D+i].first);
+        }
+        
+        if(minimum >= heap[current].first)
             break;
         int next = current*D+1;
-        while(heap[next] != maksimum)
+        while(heap[next].first != minimum)
         {
             next++;
         }
@@ -61,21 +87,20 @@ std::pair<const KeyType, ValueType> Heap4<KeyType, ValueType>::pop() noexcept
 }
 
 template <typename KeyType, typename ValueType>
-void Heap4<KeyType, ValueType>::insert(KeyType const & key, ValueType const & value)
+void Heap3<KeyType, ValueType>::insert(KeyType const & key, ValueType const & value)
 {
     insert(make_pair(key, value));
 }
 
 template <typename KeyType, typename ValueType>
-void Heap4<KeyType, ValueType>::insert(std::pair<const KeyType, ValueType> const & key_value)
+void Heap3<KeyType, ValueType>::insert(std::pair<KeyType, ValueType> const & key_value)
 {
-    cout<<"insert\n";
     heap.push_back(key_value);
     
     int myNumber = heap.size()-1;
     int parent = (myNumber-1)/D;
 
-    while( myNumber != 0 && heap[parent] < heap[myNumber] )
+    while( myNumber != 0 && heap[parent] > heap[myNumber] )
     {
         std::pair<KeyType, ValueType> tmp = heap[parent];
         heap[parent] = make_pair(heap[myNumber].first, heap[myNumber].second);
@@ -90,7 +115,7 @@ void Heap4<KeyType, ValueType>::insert(std::pair<const KeyType, ValueType> const
 
 
 template <typename KeyType, typename ValueType>
-int Heap4<KeyType, ValueType>::getHeight()
+int Heap3<KeyType, ValueType>::getHeight()
 {
     if(heap.empty())
         return 0;
@@ -98,7 +123,7 @@ int Heap4<KeyType, ValueType>::getHeight()
     int i = 0;
     while( i < heap.size()-1 )
     {
-        i = i * 4 + 4 ;
+        i = i * 3 + 3 ;
         height ++;
     }
     return height;
--- Kopce/src/main.cpp
+++ Kopce/src/main.cpp
@@ -1,12 +1,88 @@
+#include <stdexcept>
 #include <iostream>
-#include <heap.hpp>
+#include <cassert>
+#include "heap.hpp"
 #include <visualization.hpp>
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
-    Heap4<int,int> heap;
-    heap.insert(5,10);
-    cout<< heap.heap[0].first << " "<< heap.heap[0].second<<"\n";
-    cout<<"heap\n";
-    visualizeHeap(heap);
+void unit_test() 
+{
+    Heap3<int,int> heap;
+
+    // pusty kopiec
+    assert(heap.empty());
+    assert(heap.size() == 0);
+
+    bool exception_thrown = false;
+    try 
+	{
+        heap.peek();
+    } 
+	catch(const std::logic_error&) 
+	{
+        exception_thrown = true;
+    }
+    assert(exception_thrown);
+
+    // dodawanie elementów
+    heap.insert(10, 10);	// [10]
+    assert(heap.peek().first == 10);
+
+    //visualizeHeap(heap);
+    cout << "\n";
+
+    heap.insert(50, 50);	// [10, 50]
+    //visualizeHeap(heap);
+    cout << "\n";
+    heap.insert(100, 100);  // [10, 50, 100]
+    //visualizeHeap(heap);
+    cout << "\n";
+    heap.insert(40, 40);	// [10, 40, 50, 100]
+    //visualizeHeap(heap);
+    cout << "\n";
+    heap.insert(20, 20);	// [10, 20, 40, 50, 100]
+
+    //visualizeHeap(heap);
+
+    assert(heap.peek().first == 10);
+
+    heap.insert(5, 5);		// [5, 10, 20, 40, 50, 100]
+    assert(heap.peek().first == 5);
+
+	heap.print(std::cout);
+
+    assert(!heap.empty());
+    assert(heap.size() == 6);
+
+    // pobieranie elementów
+    heap.pop();				// [10, 20, 40, 50, 100]
+    assert(heap.peek().first == 10);
+    assert(heap.size() == 5);
+
+    heap.pop();				// [20, 40, 50, 100]
+    assert(heap.peek().first == 20);
+
+    heap.pop();				// [40, 50, 100]
+    assert(heap.peek().first == 40);
+
+    heap.pop();				// [50, 100]
+    assert(heap.peek().first == 50);
+
+    heap.pop();				// [100]
+    assert(heap.peek().first == 100);
+
+    heap.pop();
+    assert(heap.empty());
+}
+
+int main()
+{
+	unit_test();
+
+    /*Heap3<int,int> heap;
+    heap.insert(40, 10);
+    heap.insert(50, 10);
+    heap.insert(100, 10);
+    cout << heap.peek().first << "\n";
+    heap.pop();
+    cout << heap.peek().first << "\n";*/
 }
