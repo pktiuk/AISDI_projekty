8caf7e1e03d5e89330a378137467fc920d790426
Tomasz Michal Slojewski <tslojews@loch-61.lochy.elka.pw.edu.pl>
2020-01-10 10:52:16 +0100

    and again

--- Wzorzec/include/searching.h
+++ Wzorzec/include/searching.h
@@ -77,10 +77,12 @@ std::vector<int>findBM(std::string const & str, std::string const & text)
             
         }
         else
+        {
             if( j - shiftBM[indeks( text[ i + j ])] > 0 )
                 i += j - shiftBM[indeks( text[ i + j ])];
             else
                 ++i;    
+        }
     }
     return v;
 }
